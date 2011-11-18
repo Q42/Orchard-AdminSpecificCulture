f9114b96f3538051eb05ab6229cc08e970723d13
unknown <lukas@.q42.local>
2011-11-18 10:04:56 +0100

    w00t f1rst p0st

---
+++ Handlers/AdminCultureSettingsPartHandler.cs
@@ -0,0 +1,22 @@
+﻿using Orchard.ContentManagement.Handlers;
+using Orchard.Data;
+using Orchard.Environment.Extensions;
+using Q42.AdminSpecificCulture.Models;
+
+namespace Q42.AdminSpecificCulture.Handlers
+{
+
+  public class AdminCultureSettingsPartHandler : ContentHandler
+  {
+
+    // contenthandler: http://www.orchardproject.net/docs/Understanding-content-handlers.ashx
+
+    public AdminCultureSettingsPartHandler(IRepository<AdminCultureSettingsPartRecord> repository)
+    {
+      Filters.Add(new ActivatingFilter<AdminCultureSettingsPart>("Site"));
+      Filters.Add(StorageFilter.For(repository));
+    }
+
+  }
+
+}
---
+++ Migrations.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+using Q42.AdminSpecificCulture.Models;
+
+namespace Q42.AdminSpecificCulture
+{
+  public class Migrations : DataMigrationImpl
+  {
+
+    public int Create()
+    {
+      SchemaBuilder.CreateTable(typeof(AdminCultureSettingsPartRecord).Name, table => table
+        .ContentPartRecord()
+        .Column("AdminCulture", DbType.String)
+      );
+
+      ContentDefinitionManager.AlterPartDefinition(typeof(AdminCultureSettingsPartRecord).Name, part => part.Attachable(false));
+
+      return 1;
+    }
+
+  }
+}
